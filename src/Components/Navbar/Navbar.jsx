246d3f74a9dd96baaa8ff535c576316080291ef7
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-24 18:21:46 +0530

    created E-com

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+
+import logo from "../../assets/logo.jpg";
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src= {logo} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About us</li>
+                <li>Campus</li>
+                <li>Testimonials</li>
+                <li>Contact us</li>
+            </ul>
+        </nav>
+
+    );
+}
+
+export default Navbar
