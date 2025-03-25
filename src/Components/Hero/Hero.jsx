2136e364659b4cd43ebc907321b62e68ebf3bbfe
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-25 23:39:22 +0530

    Created Hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Hero.css";
+
+
+
+
+const Hero = () => {
+    return (
+        <div className = "hero">
+           <div className= "hero-text">
+              <h1>Empowering Education for a Brighter Future</h1>
+              <p>
+                Education is the key to unlocking endless possibilities. When we invest in quality learning, we empower individuals 
+                with the knowledge and skills they need to succeed.A strong education system not only helps people grow but also builds 
+                a better society.By ensuring access to education for all, we create a future filled with opportunities, innovation, and progress.
+              </p>
+              <button className= "btn"></button>
+           </div>
+        </div>
+    );
+}
+
+export default Hero
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./Navbar.css";
 
-import logo from "../../assets/logo.jpg";
+import logo from "../../assets/6.jpg";
 
 
 const Navbar = () => {
     return (
-        <nav>
-            <img src= {logo} alt="" />
+        <nav className = "container">
+            <img src= {logo} alt="" className="logo" />
             <ul>
                 <li>Home</li>
-                <li>Program</li>
+                <li>Programme</li>
                 <li>About us</li>
                 <li>Campus</li>
                 <li>Testimonials</li>
-                <li>Contact Us</li>
+                <li><button className= "btn"> Contact Us </button></li> 
                 
             </ul>
         </nav>
