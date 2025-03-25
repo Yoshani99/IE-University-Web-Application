fd3556006f1ebf07427cad248fd298a7c0caebef
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-26 00:22:02 +0530

    finished Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./Hero.css";
-
+import dark_arrow from "../../assets/10.jpg"
 
 
 
 const Hero = () => {
     return (
-        <div className = "hero">
+        <div className = "hero container">
            <div className= "hero-text">
               <h1>Empowering Education for a Brighter Future</h1>
               <p>
-                Education is the key to unlocking endless possibilities. When we invest in quality learning, we empower individuals 
+                Education is the key to unlocking endless possibilities.When we invest in quality learning, we empower individuals 
                 with the knowledge and skills they need to succeed.A strong education system not only helps people grow but also builds 
                 a better society.By ensuring access to education for all, we create a future filled with opportunities, innovation, and progress.
               </p>
-              <button className= "btn"></button>
+              <button className= "btn">Explore more <img src={dark_arrow}alt=""/></button>
            </div>
         </div>
     );
