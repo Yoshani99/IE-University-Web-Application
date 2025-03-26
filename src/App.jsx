1da8068483cc40e0d37603d0a4a1bff071dd3bff
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-27 01:07:13 +0530

    Finished Title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programme from "./Components/Programme/Programme";
+import Title from "./Components/Title/Title"
 
 const App = () => {
   return(
@@ -9,12 +10,14 @@ const App = () => {
       <Navbar/>
       <Hero/>
 
+
       <div className="container">
+        <Title subTitle = "Our Program" title="What We Offer"/>
         <Programme/>
       </div>
       
     </div>
   )
-}
+} 
 
 export default App
