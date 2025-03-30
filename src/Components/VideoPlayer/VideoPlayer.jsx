93c648c8b28f35febd83356812eba840ef95aed3
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-30 22:02:24 +0530

    Finished VideoPlayer

---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import "./VideoPlayer.css"
+import video from "../../assets/University-video.mp4"
+
+
+const VideoPlayer = ({playState, setPlayState}) => {
+
+
+
+    
+    const player = useRef(null)
+     
+    const closePlayer =(e)=>{
+        if(e.target === player.current){
+            setPlayState(false)
+        }
+    }
+
+  return(
+    <div className={`video-player ${playState? "" : "hide"}`} ref={player}onClick={closePlayer}>
+        <video src={video} autoPlay muted controls></video>
+    </div>
+  )
+}
+
+
+
+export default VideoPlayer;
