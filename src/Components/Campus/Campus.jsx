2223caf2831d771a089ad7919be617989b9d47e2
Yoshani99 <yoshaniavi99@gmail.com>
2025-03-30 20:12:18 +0530

    Finished subedit

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -4,8 +4,8 @@ import gallery_1 from "../../assets/gallery-01.webp"
 import gallery_2 from "../../assets/gallery-02.webp"
 import gallery_3 from "../../assets/gallery-03.avif"
 import gallery_4 from "../../assets/gallery-04.avif"
-import gallery_5 from "../../assets/gallery-05.avif"
-import gallery_6 from "../../assets/gallery-06.jpg"
+// import gallery_5 from "../../assets/gallery-05.avif"
+// import gallery_6 from "../../assets/gallery-06.jpg"
 import white_arrow from "../../assets/white-arrow.png"
 
 
@@ -17,8 +17,8 @@ const Campus =() => {
                 <img src={gallery_2} alt="" />
                 <img src={gallery_3} alt="" />
                 <img src={gallery_4} alt="" />
-                <img src={gallery_5} alt="" />
-                <img src={gallery_6} alt="" />
+                {/* <img src={gallery_5} alt="" />
+                <img src={gallery_6} alt="" /> */}
             </div>
 
             <button className= "btn dark-btn" >  See more  <img src={white_arrow} /> </button>
